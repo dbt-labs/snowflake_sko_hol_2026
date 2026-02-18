f9f47752a60579a87f7a49b9cc12b47198293946
Luis Leon <luis.leon@dbtlabs.com>
2026-02-18 00:02:23 +0000

    adding timespine

---
+++ models/hed/semantic_models/metricflow_time_spine.sql
@@ -0,0 +1,22 @@
+-- models/metricflow_time_spine.sql
+-- This model creates a time spine table required by the dbt Semantic Layer
+-- It generates a row for each day/hour/etc to support time-based metric queries
+
+{{
+  config(
+    materialized='table',
+    tags=['metricflow']
+  )
+}}
+
+WITH date_spine AS (
+  {{ dbt_utils.date_spine(
+      datepart="day",
+      start_date="cast('2020-01-01' as date)",
+      end_date="cast('2030-12-31' as date)"
+  ) }}
+)
+
+SELECT
+  date_day AS date_day
+FROM date_spine
